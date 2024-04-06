97b0e6871772b67cbd9014bbea87e93c4d6573fc
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-06 01:25:33 -0400

    Completed lesson_ 3 on variables and lesson_5.

---
+++ lesson_3.py
@@ -0,0 +1,10 @@
+age = 20
+temperature = 25.39
+country = "Australia"
+is_day = True
+
+print(country)
+
+country = 900
+
+print(country)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from turtle import *
-
-forward(100)
+country_name = "America"
+population = 320000000
+landlocked = False
+border_size = 5525
