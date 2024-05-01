452bb5abc2d9f84953b573bae88c6713fe4683d6
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-01 16:36:51 -0400

    Added a pop functionality and called onkey and listen functions

--- main.py
+++ main.py
@@ -28,7 +28,22 @@ def draw_balloon():
     color("red")
     dot(diameter)
 
+def inflate_balloon():
+    global diameter
+    diameter = diameter + 10
+    draw_balloon()
+
+    if diameter >= pop_diameter:
+        clear()
+        diameter = 40
+        write("POP!")
+
+
 
 
 draw_balloon()
+
+onkey(inflate_balloon, "Up")
+listen()
+
 done()
