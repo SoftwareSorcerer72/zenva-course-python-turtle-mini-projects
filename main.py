1ae58bdff51451d9994f7359ae798dd4ee4e421b
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-01 04:06:49 -0400

    Made code diagram

---
+++ main.py
@@ -0,0 +1,8 @@
+# Be able to detect inputs.
+# Require multiple inputs to pop the ballon.
+# Utilize variables, conditions and functions.
+
+#Code Diagram for project below 
+
+# https://www.codediagram.io/app/shares?token=a645c4a9
+
