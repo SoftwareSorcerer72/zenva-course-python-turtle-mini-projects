5a14d53ecde4ffc4e3ab3ac09f9f33ff1caf7c5e
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-01 04:14:17 -0400

    Added pseudo code and fixed some typos

--- main.py
+++ main.py
@@ -6,3 +6,14 @@
 
 # https://www.codediagram.io/app/shares?token=a645c4a9
 
+# pseudo code
+
+# Start 
+# Draw the balloon 
+# Has the key been pressed? 
+#     Has the ballon reached max size? 
+#         Clear the ballon
+#     If not 
+#         Increase the balloon size
+#         Draw the balloon
+# End
