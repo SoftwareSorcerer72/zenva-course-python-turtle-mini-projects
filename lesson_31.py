9450ec2828d611e4c1aea4e181c6f68fbaeb1ec4
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-06 22:56:17 -0400

    Completed lesson 32

---
+++ lesson_31.py
@@ -0,0 +1,4 @@
+num = 0
+for x in range(100):
+    num = num + 1
+    print(num)
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-num = 0
-for x in range(100):
-    num = num + 1
-    print(num)
+from turtle import *
+
+
+
+# def move_and_turn (angle):
+#     num = 0
+#     for x in range(8):
+#         num = num + 1
+#         forward(50)
+#         right(angle)
+
+
+# move_and_turn(45) 
+
+def move_and_turn (angle):
+    forward(50)
+    right(angle)
+
+for x in range(12):
+    move_and_turn(30)
+
+
+
+done()
