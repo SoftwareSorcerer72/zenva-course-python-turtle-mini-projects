d1821b99284a72c86de159ff5514c552f151ae52
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-08 03:53:13 -0400

    Added pseudo code and a code diagram to turtle game file

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# START
+# Set the background color to ORANGE
+# Draw the ocean
+# Move the turtle to starting position
+
+# Pressed MOVE key?
+#     Move turtle in that direction
+
+#     Have we reached the goal?
+#         Hide the turtle and disable controls
+#         Write "YOU WIN!" on-screen
+
+#         END
+
+
+
+
+
+
+
+
+
+# https://www.codediagram.io/app/shares?token=a4eff907
