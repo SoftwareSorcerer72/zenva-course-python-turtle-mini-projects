4985f61499dedfe344f774bc1471f1b923ea8ff5
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-05-15 20:33:54 -0400

    Added moving functions with onkey and listener functions

--- main.py
+++ main.py
@@ -1,8 +1,61 @@
 from turtle import *
 
+
+speed(0)
+
+
 move_distance = 20
 
 bgcolor("#D2691E")
+
+penup()
+goto(100, 200)
+pendown()
+
+
+color("blue")
+begin_fill()
+goto(300, 200)
+goto(300, -200)
+goto(100, -200)
+goto(100, 200)
+end_fill()  
+
+penup()
+goto(-200, 0)
+shape("turtle")
+color("green")
+
+def move_up():
+    setheading(90)
+    forward(move_distance)
+
+move_up()
+
+
+def move_down():
+    setheading(270)
+    forward(move_distance)
+
+move_down()
+
+def move_left():
+    setheading(180)
+    forward(move_distance)
+
+move_left()
+
+def move_right():
+    setheading(0)
+    forward(move_distance)
+move_right()
+
+onkey(move_up, "Up")
+onkey(move_down, "Down")
+onkey(move_left, "Left")
+onkey(move_right, "Right")
+listen()
+
 
 done()
 
